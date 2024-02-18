8169e82b5c74d89ba8f4bf3e6e2a360a1d6b2375
wangzixuan.wzxuan <wangzixuan.wzxuan@bytedance.com>
2024-02-18 16:57:40 +0800

    add news Commentator prompt
    (cherry picked from commit 903ed0a864496d74e6a4f1ce2f9a6396cadaff1d)

---
+++ src/pages/prompt/2001.tsx
@@ -0,0 +1,97 @@
+import React from "react";
+import PromptPage from "../_components/PromptPage";
+import { AuthProvider } from "@site/src/pages/_components/AuthContext";
+
+const prompt = {
+  "zh": {
+    "title": "新闻时事评论",
+    "prompt": "我希望你充当一名精通多领域知识的专家，包括但不限于经济, 政治, 社会, 医学, 国际关系, 环境等。你需要分析一条新闻事件，并完成以下工作\n1. 向不懂得相关领域的人简要解释新闻内容\n2. 给出你对这条新闻的评论\n3. 这个事件后续的发展预测\n4. 这个事件会对我们生活/国家/环境/社会等造成什么影响\n新闻内容如下:\n[]",
+    "description": "我希望你充当一名精通多领域知识的专家，包括但不限于经济, 政治, 社会, 医学, 国际关系, 环境等。你需要分析一条新闻事件，并完成以下工作\n1. 向不懂得相关领域的人简要解释新闻内容\n2. 给出你对这条新闻的评论\n3. 这个事件后续的发展预测\n4. 这个事件会对我们生活/国家/环境/社会等造成什么影响\n新闻内容如下:\n[]",
+    "remark": "对新闻时事评论"
+  },
+  "en": {
+    "title": "News Commentator",
+    "prompt": "I want you to be an expert in many fields, including but not limited to economics, politics, society, medicine, international relations, environment, etc. You need to analyze a news event and complete the following tasks\n1. Briefly explain the news to people who don't understand the relevant fields\n2. Give your comments on this news\n3. The follow-up development forecast of this event\n4. What impact will this event have on our life/country/environment/society, etc.\nThe content of the news is as follows:\n[]",
+    "description": "I want you to be an expert in many fields, including but not limited to economics, politics, society, medicine, international relations, environment, etc. You need to analyze a news event and complete the following tasks\n1. Briefly explain the news to people who don't understand the relevant fields\n2. Give your comments on this news\n3. The follow-up development forecast of this event\n4. What impact will this event have on our life/country/environment/society, etc.\nThe content of the news is as follows:\n[]",
+    "remark": "Comments on news and current affairs"
+  },
+  "ja": {
+    "title": "null",
+    "prompt": "null",
+    "description": "null",
+    "remark": "null"
+  },
+  "ko": {
+    "title": "null",
+    "prompt": "null",
+    "description": "null",
+    "remark": "null"
+  },
+  "es": {
+    "title": "null",
+    "prompt": "null",
+    "description": "null",
+    "remark": "null"
+  },
+  "fr": {
+    "title": "null",
+    "prompt": "null",
+    "description": "null",
+    "remark": "null"
+  },
+  "de": {
+    "title": "null",
+    "prompt": "null",
+    "description": "null",
+    "remark": "null"
+  },
+  "it": {
+    "title": "null",
+    "prompt": "null",
+    "description": "null",
+    "remark": "null"
+  },
+  "ru": {
+    "title": "null",
+    "prompt": "null",
+    "description": "null",
+    "remark": "null"
+  },
+  "pt": {
+    "title": "null",
+    "prompt": "null",
+    "description": "null",
+    "remark": "null"
+  },
+  "hi": {
+    "title": "null",
+    "prompt": "null",
+    "description": "null",
+    "remark": "null"
+  },
+  "ar": {
+    "title": "null",
+    "prompt": "null",
+    "description": "null",
+    "remark": "null"
+  },
+  "bn": {
+    "title": "null",
+    "prompt": "null",
+    "description": "null",
+    "remark": "null"
+  },
+  "website": null,
+  "tags": [
+    "favorite",
+    "comments"
+  ],
+  "id": 2001,
+  "weight": 14572
+};
+
+function PromptDetail() {
+  return <AuthProvider><PromptPage prompt={prompt} /></AuthProvider>;
+}
+
+export default PromptDetail;

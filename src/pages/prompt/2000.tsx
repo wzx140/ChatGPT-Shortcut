af3b0a0f7b9b95b2b7694658b40de99127a93118
wzx <masterwangzx@gmail.com>
2024-02-18 23:51:43 +0800

    modify prompt
    (cherry picked from commit bd5dfdeff4ae56a9e5fed46f2aad81ab4b3c022f)

--- src/pages/prompt/2000.tsx
+++ src/pages/prompt/2000.tsx
@@ -5,14 +5,15 @@ import { AuthProvider } from "@site/src/pages/_components/AuthContext";
 const prompt = {
   "zh": {
     "title": "笔记整理",
-    "prompt": "我希望你充当一名笔记整理助理, 你将会面对[书名]这本书中的多条碎片化笔记, 你的任务是将碎片化的笔记按照逻辑性整理分类为多个标题, 每个标题下用结构化的文字表达。笔记一共分为[1]部分, 这是第[1]部分: []",
-    "description": "我希望你充当一名笔记整理助理, 你将会面对[书名]这本书中的多条碎片化笔记, 你的任务是将碎片化的笔记按照逻辑性整理分类为多个标题, 每个标题下用结构化的文字表达。笔记一共分为[1]部分, 这是第[1]部分: []",
+    "prompt": "我希望你充当一名笔记整理助理, 你将会面对[书名]这本书中的多条笔记, 你的任务是将笔记按照逻辑进行整理, 注意用结构化的文字表达。笔记一共分为[1]部分, 这是第[1]部分: []",
+    "description": "我希望你充当一名笔记整理助理, 你将会面对[书名]这本书中的多条笔记, 你的任务是将笔记按照逻辑进行整理, 注意用结构化的文字表达。笔记一共分为[1]部分, 这是第[1]部分: []",
     "remark": "对笔记进行分类整理"
   },
   "en": {
-    "title": "Note organization",
-    "prompt": "I hope you can serve as a note organizing assistant. You will be faced with multiple fragmented notes in the book [title]. Your task is to logically organize and categorize the fragmented notes into multiple headings, with structured text used to express them under each heading. The notes are divided into [1] sections, which is the [1] section: []",
-    "remark": "Classify and organize notes"
+    "title": "null",
+    "prompt": "null",
+    "description": "null",
+    "remark": "null"
   },
   "ja": {
     "title": "null",
